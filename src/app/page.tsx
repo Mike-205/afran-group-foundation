7b936d1619365642249bc21c68904f4cc08f65d3
Mike Mwongela <mikemwongela1025@gmail.com>
2025-09-10 13:49:53 +0300

    So it begins...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="text-lg primary-500">Hello Word</div>
+    <div className={'text-2xl text-primary-700 font-black inset-shadow-lg shadow-primary-500/20'}>Hello World</div>
   );
 }
